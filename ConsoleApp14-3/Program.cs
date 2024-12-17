78f83aed61728ad43a0c15280722524389b4c0cc
muxazzXJohny <maracoco334@gmail.com>
2024-12-17 09:50:55 +0200

    Add files via upload

---
+++ ConsoleApp14-3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14_3
+{
+    class Program
+    {
+
+        /// <summary>
+        /// Задание 1. Вывести на экран в столбик первые 10 натуральных чисел (циклы с условием, цикл с параметром)
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Func();
+            Console.ReadKey();
+        }
+        static void Func()
+        {
+            for (int x = 1; x <= 5; x++)
+            {
+                Console.WriteLine("Привет!");
+            }
+        }
+    }
+}
---
+++ ConsoleApp14-4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("xxxxxxxxxxxxxxxxxxxx");
+                               x
+                               x
+                               x
+                               x
+            Console.WriteLine("xxxxxxxxxxxxxxxxxxxx");
+            Console.WriteLine("xxxxxxxxxxxxxxxxxxxx");
+            Console.WriteLine("xxxxxxxxxxxxxxxxxxxx");
+            Console.WriteLine("xxxxxxxxxxxxxxxxxxxx");
+            Console.ReadKey();
+
+
+        }
+    }
+}

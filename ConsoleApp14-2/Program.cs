7e74dce7848303096234a33c182ca0c2b8bd80c3
muxazzXJohny <maracoco334@gmail.com>
2024-12-23 20:30:17 +0200

    Add files via upload

---
+++ ConsoleApp14-2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.ReadKey();
+        }
+        static void Func()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int x = 1;
+            do
+            {
+                Console.WriteLine(x);
+                x++;
+            }
+            while (x <= n);
+        }
+    }
+}
